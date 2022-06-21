d111a0e1cfb16106eea35a13e1721e00215522a4
pidobear <pidobear@live.com>
2022-06-21 23:29:55 +0800

    1 binaries uploaded

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.css'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+  playMusic: Boolean = false;
+  audio = new Audio();
+  toggleMusic() {
+    if (this.playMusic) {
+      this.playAudio();
+      this.playMusic = true;
+    } else {
+      this.stopAudio();
+      this.playMusic = false;
+    }
+  }
+  playAudio() {
+    this.audio.src = '../../../assets/music/Nico Staf _ Fast and Run.mp3';
+    this.audio.load();
+    this.audio.play();
+  }
+  stopAudio() {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+  }
+}
