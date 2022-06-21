726fa0f8de9990ba858e1c454e13e5c4a9a116eb
pidobear <pidobear@live.com>
2022-06-22 00:41:42 +0800

    destroy/stop audio or navigating

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.css'],
 })
-export class LandingComponent implements OnInit {
-  playMusic: Boolean = false;
+export class LandingComponent implements OnInit, OnDestroy {
+  playMusic: Boolean = true;
   audio = new Audio();
   ngOnInit() {
     this.playAudio();
   }
+  ngOnDestroy() {
+    this.stopAudio();
+  }
   toggleMusic() {
     if (this.playMusic) {
-      this.stopAudio();
+      this.muteAudio();
       this.playMusic = false;
     } else {
       this.unmuteAudio();
@@ -28,13 +31,19 @@ export class LandingComponent implements OnInit {
       'https://github.com/pidobear/testt1roll9q123123qweqw31/raw/master/src/assets/music/Nico%20Staf%20_%20Fast%20and%20Run.mp3';
     this.audio.load();
     this.audio.play();
+    this.audio.loop = true;
     this.audio.volume = 0.7;
     // console.log('play');
   }
-  stopAudio() {
+  muteAudio() {
     this.audio.volume = 0;
     // this.audio.pause();
     // this.audio.currentTime = 0;
     // console.log('stop');
+  }
+  stopAudio() {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    console.log('stop');
   }
 }
